11b44dfb782cbcd7602cc68f74a480e1aef0be5b
Yamangulov Andrey <yamangulov@gmail.com>
2021-07-05 07:26:09 +0300

    Доработка авторизации с учетом добавленных пользователей.

--- src/main/java/org/example/app/services/LoginService.java
+++ src/main/java/org/example/app/services/LoginService.java
@@ -2,6 +2,8 @@ package org.example.app.services;
 
 import org.apache.log4j.Logger;
 import org.example.web.dto.LoginForm;
+import org.example.web.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,8 +11,24 @@ public class LoginService {
 
     private Logger logger = Logger.getLogger(LoginService.class);
 
+    private final RegisterService registerService;
+
+    @Autowired
+    public LoginService(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
     public boolean authenticate(LoginForm loginFrom) {
         logger.info("try auth with user-form: " + loginFrom);
-        return loginFrom.getUsername().equals("root") && loginFrom.getPassword().equals("123");
+        boolean resultAuthAdmin = loginFrom.getUsername().equals("root") && loginFrom.getPassword().equals("123");
+        boolean resultAuthUser = false;
+        for (User user : registerService.getAllUsers()) {
+            if (user.getUser().equals(loginFrom.getUsername())
+                    && user.getPassword().equals(loginFrom.getPassword())) {
+                resultAuthUser = true;
+                break;
+            }
+        }
+        return resultAuthAdmin || resultAuthUser;
     }
 }
